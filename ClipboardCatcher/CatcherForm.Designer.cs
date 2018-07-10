f2a9062bee20c4c2a8e7c12419ae0f7c763736de
emadsaber <omda.sharkawy@gmail.com>
2018-07-10 20:09:36 +0200

    organized solution issue #8

---
+++ ClipboardCatcher/CatcherForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace ClipboardCatcher
+{
+    partial class CatcherForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CatcherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "CatcherForm";
+            this.Text = "CatcherForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CatcherForm_FormClosing);
+            this.Load += new System.EventHandler(this.CatcherForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ ClipboardCatcher/CatcherForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipboardCatcher
+{
+    public partial class CatcherForm : Form
+    {
+        #region Externs
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        private const int WM_DRAWCLIPBOARD = 0x0308;
+        private IntPtr _clipboardViewerNext;
+
+        #endregion
+
+        #region Constructors
+        public CatcherForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Methods
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == WM_DRAWCLIPBOARD)
+            {
+                IDataObject iData = Clipboard.GetDataObject();
+                if (iData.GetDataPresent(DataFormats.Text))
+                {
+                    string text = (string)iData.GetData(DataFormats.UnicodeText);
+                    //Fire event
+                    this.TextCopied.Invoke(this, text);
+                }
+                else if (iData.GetDataPresent(DataFormats.Bitmap))
+                {
+                    Bitmap image = (Bitmap)iData.GetData(DataFormats.Bitmap);
+                    //Fire event
+                    this.ImageCopied.Invoke(this, image);
+                }
+            }
+        }
+        private void CatcherForm_Load(object sender, EventArgs e)
+        {
+            this._clipboardViewerNext = SetClipboardViewer(this.Handle);
+        }
+        private void CatcherForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ChangeClipboardChain(this.Handle, _clipboardViewerNext);
+        }
+        #endregion
+
+        #region Events
+
+        public event EventHandler<string> TextCopied = new EventHandler<string>((x, y) => { return; });
+        public event EventHandler<Bitmap> ImageCopied = new EventHandler<Bitmap>((x, y) => { return; });
+
+        #endregion
+    }
+}
